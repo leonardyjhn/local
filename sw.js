81bbd13b2112a39edd56837a50dfa2ba1128cfd6
Leonard <155848444+leonardyjhn@users.noreply.github.com>
2025-07-17 16:19:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,22 @@
-const CACHE_NAME = 'rifas-sucre-cache-v1';
+const CACHE_NAME = 'rifas-sucre-cache-v3'; // Incrementa la versión
 const urlsToCache = [
   '/',
   '/index.html',
   '/styles.css',
   '/script.js',
   '/manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+  '/js/html2canvas.min.js',
+  '/css/all.min.css',
+  '/webfonts/fa-solid-900.woff2',
+  '/webfonts/fa-solid-900.ttf',
+  '/webfonts/fa-brands-400.woff2',
+  '/webfonts/fa-brands-400.ttf',
+  '/webfonts/fa-regular-400.woff2',
+  '/webfonts/fa-regular-400.ttf',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/icons/icon-maskable-192.png',
+  '/icons/icon-maskable-512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -22,11 +32,11 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // Cache-first strategy
         if (response) {
           return response;
         }
         return fetch(event.request);
-      }
-    )
+      })
   );
 });
